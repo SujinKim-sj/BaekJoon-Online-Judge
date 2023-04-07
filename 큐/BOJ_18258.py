41ddbf6af3ed3036187f64452b99a30fcffa2033
SujinKim-sj <sjsjkim0@gmail.com>
2023-04-07 17:50:00 +0900

    18258번 : 큐2

---
+++ 큐/BOJ_18258.py
@@ -0,0 +1,34 @@
+# Queue
+# 18258번. 큐2
+
+import sys
+from collections import deque
+n = int(sys.stdin.readline())
+queue = deque([])
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if not queue:
+            print(-1)
+        else:
+            print(queue.popleft())
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if not queue:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[-1])
