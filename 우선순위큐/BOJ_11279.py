44671699bc32a50236e8e3032f9ef6a1a734de7c
SujinKim-sj <sjsjkim0@gmail.com>
2023-04-25 15:38:58 +0900

    11279번 : 최대 힙

---
+++ 우선순위큐/BOJ_11279.py
@@ -0,0 +1,18 @@
+# Priority Queue
+# 11279번. 최대 힙
+
+import sys
+import heapq
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    if num != 0 :
+        heapq.heappush(heap, -num)
+    else:
+        try:
+            print(-1 * heapq.heappop(heap))
+        except:
+            print(0)
