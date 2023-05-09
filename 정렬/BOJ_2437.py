d967d62e5ec01b854c8e9960f4d6ac60322bda33
SujinKim-sj <sjsjkim0@gmail.com>
2023-05-09 15:08:32 +0900

    2750번, 2751번 : 수 정렬하기

---
+++ 정렬/BOJ_2437.py
@@ -0,0 +1,15 @@
+# Sort
+# 2437번. 저울
+
+n = int(input())
+weight = list(map(int, input().split()))
+weight.sort()
+
+answer = 1
+
+for i in weight:
+    if answer < i:
+        break
+    answer += i
+
+print(answer)
