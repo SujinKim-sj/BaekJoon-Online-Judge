ea42178a04dbc5299edab72a29f04b407cd3bca0
SujinKim-sj <sjsjkim0@gmail.com>
2023-05-11 22:14:42 +0900

    1439번 : 뒤집기

---
+++ 탐욕법/BOJ_1439.py
@@ -0,0 +1,20 @@
+# Greedy, 문자열
+# 1439번. 뒤집기
+
+s = input()
+count0 = 0
+count1 = 0
+
+if s[0] == '1':
+    count0 += 1
+else:
+    count1 += 1
+
+for i in range(len(s) - 1):
+    if s[i] != s[i + 1]:
+        if s[i + 1] == '1':
+            count0 += 1
+        else:
+            count1 += 1
+
+print(min(count0, count1))
